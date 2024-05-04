348b2152fc8c2268901ca25d2d978243970ac680
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2024-05-04 16:49:44 +0530

    dummy data with styling of card

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -10,41 +10,12 @@ import {
 import "../styles/dashboard.css";
 import CircularProgressWithLabel from "./CircularProgressWithLabel";
 
-const JobListings = () => {
-  const jobData = [
-    {
-      company: "narrative (yc w23)",
-      role: "Founding Fullstack Engineer",
-      location: "India",
-      salary: "₹15 - 40 LPA",
-      description:
-        "About us Narrative is making AI powered data tools for logistics. We standardize and find errors in shipping invoice data for large companies.",
-      experience: "3 years",
-      daysAgo: 13,
-    },
-    {
-      company: "Business on Bot",
-      role: "Fullstack Engineer",
-      location: "Bangalore",
-      salary: "₹15 - 20 LPA",
-      description:
-        "About us We are on a mission to help 30 million+ D2C brands and SMBs with automated sales and user acquisition on WhatsApp in India. In the first half of 2021, Businessonbot has grown 145% M-o-M in MRR and targets a massive wave of D2C businesses in India. This year, we'd like to become the 1st and largest WhatsApp SaaS player of its kind in India. There's never been a more exciting time to be in Businessonbot.",
-      experience: "1 year",
-      daysAgo: 8,
-    },
-    {
-      company: "CertifyOS",
-      role: "FullStack Engineer",
-      location: "India",
-      salary: "₹40 - 60 LPA",
-      description:
-        "About us CertifyOS is a first-of-its-kind provider intelligence platform, powered by API integrations and hundreds of verified data points. We unlock insights and power performance for clinicians, teams and organizations, with frictionless licensing and enrollment, one-click credentialing and real-time network monitoring at your fingertips",
-      experience: "2 years",
-      daysAgo: 22,
-      match: 67,
-    },
-  ];
+import * as data from "../data/jobList.json";
+function getRandomValue() {
+  return Math.floor(Math.random() * 101);
+}
 
+const JobListings = () => {
   return (
     <Box>
       <Grid
@@ -53,7 +24,7 @@ const JobListings = () => {
         justifyContent={"center"}
         alignItems={"flex-start"}
       >
-        {jobData.map((job, index) => (
+        {data["jdList"].map((job, index) => (
           <Grid
             item
             xs={12}
@@ -62,60 +33,70 @@ const JobListings = () => {
             key={index}
             className="card-body-grid-parent"
           >
-            <Card style={{ marginBottom: 16 }} className="card-body">
+            <Card
+              style={{
+                marginBottom: 16,
+              }}
+              className="card-body"
+            >
               <div className="date-match-container">
                 <div className="date-container">
                   <Typography variant="body2" color="black" className="date">
-                    Posted ⏳ {job.daysAgo} days ago
+                    Posted ⏳ 2{job.daysAgo} days ago
                   </Typography>
                 </div>
-                {job.match && (
-                  <CircularProgressWithLabel
-                    value={job.match}
-                    variant="determinate"
-                    style={{
-                      color: "rgb(85, 239, 196)",
-                      width: "50px",
-                      height: "50px",
-                      transform: "rotate(-90deg)",
-                    }}
-                  />
-                )}
+
+                <CircularProgressWithLabel
+                  value={getRandomValue()}
+                  variant="determinate"
+                  style={{
+                    color: "rgb(85, 239, 196)",
+                    width: "50px",
+                    height: "50px",
+                    transform: "rotate(-90deg)",
+                  }}
+                />
               </div>
-              <CardContent>
+              <CardContent sx={{ justifyContent: "flex-start" }}>
                 <Box
                   component={"div"}
+                  gap={"0.5rem"}
                   sx={{
                     display: "flex",
                     flexDirection: "row",
                     justifyContent: "flex-start",
                   }}
                 >
-                  <img
-                    src="https://concrete.store/Content/images/favicon.png"
-                    alt="logo"
-                    width={25}
-                    height={40}
-                  />
+                  <img src={job.logoUrl} alt="logo" width={25} height={40} />
                   <div className="info-container">
-                    <h3>{job.company}</h3>
-                    <h2>{job.role}</h2>
+                    <h3>{job.companyName}</h3>
+                    <h2>{job.jobRole}</h2>
                     <p className="cards-sub-text">{job.location}</p>
                   </div>
                 </Box>
                 <Typography variant="body2" color="textSecondary">
-                  Estimated Salary: {job.salary}
+                  Estimated Salary: {job.minJdSalary}-{job.maxJdSalary} ✅
                 </Typography>
                 <div className="description-container">
-                  <Typography variant="body2">{job.description}</Typography>
+                  <Typography variant="body2">
+                    {job.jobDetailsFromCompany}
+                  </Typography>
                 </div>
 
                 <Typography variant="body2">
-                  Minimum Experience: {job.experience}
+                  Minimum Experience: {job.minExp ? job.minExp : "1"} years
                 </Typography>
 
-                <Button variant="contained" color="primary">
-                  Easy Apply
+                <Button
+                  variant="contained"
+                  sx={{
+                    bgcolor: "rgb(85, 239, 196)",
+                    color: "black",
+                    fontWeight: "500",
+                    width: "100%",
+                  }}
+                >
+                  ⚡ Easy Apply
                 </Button>
               </CardContent>
             </Card>
